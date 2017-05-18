0ab16538aed0a67cc960021978e3b57117773954
Dave Pearson <davep@davep.org>
2017-05-18 10:33:06 +0100

    v1.2

--- slstats.el
+++ slstats.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: games
 ;; URL: https://github.com/davep/slstats.el
 ;; Package-Requires: ((cl-lib "0.5"))
